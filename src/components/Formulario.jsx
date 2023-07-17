82947a4f59d4778de79331e9d2a8021e2f272017
Gustavo Aranciba <gusaranciba@gmail.com>
2023-07-17 15:40:13 -0300

    Mock de Paciente.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,24 +1,90 @@
-
 const Formulario = () => {
   return (
-    <div className="md:w-1/2 lg:w-2/5" >
-        <h2 className=" font-black text-3xl text-center " >
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className=" font-black text-3xl text-center ">
         Seguimiento Pacientes
-        </h2>
-        <p className=" text-lg mt-5 text-center " >
-          Añade Pacientes y {''}
-          <span className="text-indigo-600 font-bold  " >
-          Administralos
-          </span>
-        </p>
-        <form action="" className=" bg-white shadow-md " >
-          <div>
-            <label htmlFor="">Nombre de la Mascota</label>
-            <input type="text" placeholder="Nombre del Animal" />
-          </div>
-        </form>
+      </h2>
+      <p className=" text-lg mt-5 text-center mb-10 ">
+        Añade Pacientes y {""}
+        <span className="text-indigo-600 font-bold  ">Administralos</span>
+      </p>
+      <form action="" className=" bg-white shadow-md rounded-md py-10 px-5 mb-10 ">
+        <div className="mb-5">
+          <label
+            htmlFor="mascota"
+            className=" block text-gray-700 uppercase font-bold "
+          >
+            Nombre de la Mascota
+          </label>
+          <input
+            id="mascota"
+            type="text"
+            placeholder="Nombre del Animal"
+            className="border-spacing-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
+          />
+        </div>
+          <div className="mb-5">
+            <label
+              htmlFor="owner"
+              className=" block text-gray-700 uppercase font-bold "
+            >
+              Nombre del Propietario
+            </label>
+          <input
+            id="owner"
+            type="text"
+            placeholder="Nombre del  otro Animal"
+            className="border-spacing-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className=" block text-gray-700 uppercase font-bold "
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="email contacto"
+            className="border-spacing-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="alta"
+            className=" block text-gray-700 uppercase font-bold "
+          >
+            Fecha de Alta
+          </label>
+          <input
+            id="alta"
+            type="date"
+            className="border-spacing-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="sintomas"
+            className=" block text-gray-700 uppercase font-bold "
+          >
+            Síntomas
+          </label>
+          <textarea
+            id="sintomas"
+            placeholder="Describe los síntomas..."
+            className="border-spacing-2 w-full p-2 mt-2 placeholder-gray-500 rounded-md"
+          />
+        </div>
+        <input
+          type="submit"
+          className=" bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-800 cursor-pointer transition-all "
+          value="Agregar paciente"
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Formulario
+export default Formulario;
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,33 @@
+
+
+
+
+const Paciente = () => {
+  return (
+    <div className=" m-3 bg-white shadow-md px-5 py-10 rounded-xl " >
+          <p className=" font-bold mb-3 text-gray-700 uppercase " >
+            Nombre: {''}
+            <span className=" font-normal normal-case " >Hook</span>
+          </p>
+          <p className=" font-bold mb-3 text-gray-700 uppercase " >
+            Propietario: {''}
+            <span className=" font-normal normal-case " >Ron Damón</span>
+          </p>
+          <p className=" font-bold mb-3 text-gray-700 uppercase " >
+            Email: {''}
+            <span className=" font-normal normal-case " >mail@mail.com</span>
+          </p>
+          <p className=" font-bold mb-3 text-gray-700 uppercase " >
+            Fecha Alta: {''}
+            <span className=" font-normal normal-case " >20 de Junio de 2020</span>
+          </p>
+          <p className=" font-bold mb-3 text-gray-700 uppercase " >
+            Síntomas: {''}
+            <span className=" font-normal normal-case " >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis nisi temporibus consequuntur adipisci maxime laborum eos, qui animi eius. Odio maxime sapiente saepe distinctio, similique incidunt expedita ad excepturi voluptatem!</span>
+          </p>
+    </div>    
+  )
+}
+
+export default Paciente
+
