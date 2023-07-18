aeaddcfe1bc3a243a05af821d24d140b4c784798
Gustavo Aranciba <gusaranciba@gmail.com>
2023-07-18 20:19:57 -0300

    Pasando por props los datos de paciente al listado de pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,7 +2,8 @@ import Paciente from "./Paciente"
 
 
 
-const ListadoPacientes = () => {
+const ListadoPacientes = ({pacientes}) => {
+  console.log(pacientes);
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll " >
         <h2 className=" font-black text-3xl text-center " >
@@ -14,12 +15,15 @@ const ListadoPacientes = () => {
             Pacientes y Citas
           </span>
         </p>
-        <Paciente/>
-        <Paciente/>
-        <Paciente/>
-        <Paciente/>
-        <Paciente/>
-        <Paciente/>        
+
+        {pacientes.map( (paciente, index) => (
+           <Paciente
+            key={index}
+            paciente={paciente}
+           />
+          ))}
+
+        
     </div>
   )
 }
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -2,28 +2,31 @@
 
 
 
-const Paciente = () => {
+const Paciente = ({paciente}) => {
+
+  const {nombre, propietario,email, fecha, sintomas} = paciente
+
   return (
     <div className=" mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl " >
           <p className=" font-bold mb-3 text-gray-700 uppercase " >
             Nombre: {''}
-            <span className=" font-normal normal-case " >Hook</span>
+            <span className=" font-normal normal-case " >{nombre}</span>
           </p>
           <p className=" font-bold mb-3 text-gray-700 uppercase " >
             Propietario: {''}
-            <span className=" font-normal normal-case " >Ron Damón</span>
+            <span className=" font-normal normal-case " >{propietario}</span>
           </p>
           <p className=" font-bold mb-3 text-gray-700 uppercase " >
             Email: {''}
-            <span className=" font-normal normal-case " >mail@mail.com</span>
+            <span className=" font-normal normal-case " >{email}</span>
           </p>
           <p className=" font-bold mb-3 text-gray-700 uppercase " >
             Fecha Alta: {''}
-            <span className=" font-normal normal-case " >20 de Junio de 2020</span>
+            <span className=" font-normal normal-case " >{fecha}</span>
           </p>
           <p className=" font-bold mb-3 text-gray-700 uppercase " >
             Síntomas: {''}
-            <span className=" font-normal normal-case " >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis nisi temporibus consequuntur adipisci maxime laborum eos, qui animi eius. Odio maxime sapiente saepe distinctio, similique incidunt expedita ad excepturi voluptatem!</span>
+            <span className=" font-normal normal-case " >{sintomas}</span>
           </p>
     </div>    
   )
