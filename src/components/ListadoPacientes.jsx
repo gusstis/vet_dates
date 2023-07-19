e5bd597f6e8a50cf25729e61d94046e6ecb79df2
Gustavo Aranciba <gusaranciba@gmail.com>
2023-07-19 16:11:41 -0300

    Registramos un evento en botón Editar

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,7 +2,7 @@ import Paciente from "./Paciente"
 
 
 
-const ListadoPacientes = ({pacientes}) => {
+const ListadoPacientes = ({pacientes, setPaciente}) => {
   console.log(pacientes);
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll " >
@@ -23,6 +23,7 @@ const ListadoPacientes = ({pacientes}) => {
             <Paciente
               key={paciente.id}
               paciente={paciente}
+              setPaciente={setPaciente}
             />
           ))}
         </>
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -2,7 +2,7 @@
 
 
 
-const Paciente = ({paciente}) => {
+const Paciente = ({paciente, setPaciente}) => {
 
   const {nombre, propietario,email, fecha, sintomas} = paciente
 
@@ -32,6 +32,7 @@ const Paciente = ({paciente}) => {
             <button
             type="button"
             className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+            onClick={ () => setPaciente(paciente) }
             >
               Editar
             </button>
