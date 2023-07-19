61bd5fa9a9146e9931ebcc9c0de349f0fccf0135
Gustavo Aranciba <gusaranciba@gmail.com>
2023-07-19 09:58:52 -0300

    Actualizando Listado de Pacientes, mostrando texto de manera condicional

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -6,22 +6,43 @@ const ListadoPacientes = ({pacientes}) => {
   console.log(pacientes);
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll " >
-        <h2 className=" font-black text-3xl text-center " >
-            ListadoPacientes
-        </h2>
-        <p className=" text-xl mt-5 text-center mb-10 ">
-          Administra tus {''}
-          <span className="text-indigo-600 font-bold  " >
-            Pacientes y Citas
-          </span>
-        </p>
+      {pacientes && pacientes.length ? (
 
-        {pacientes.map( (paciente, index) => (
-           <Paciente
-            key={paciente.id}
-            paciente={paciente}
-           />
+        <>
+          <h2 className=" font-black text-3xl text-center " >
+            ListadoPacientes
+          </h2>
+          <p className=" text-xl mt-5 text-center mb-10 ">
+            Administra tus {''}
+            <span className="text-indigo-600 font-bold  " >
+              Pacientes y Citas
+            </span>
+          </p>
+
+          {pacientes.map( (paciente) => (
+            <Paciente
+              key={paciente.id}
+              paciente={paciente}
+            />
           ))}
+        </>
+
+      ) : (
+        <>
+        
+          <h2 className=" font-black text-3xl text-center " >
+            No hay Pacientes
+          </h2>
+          <p className=" text-xl mt-5 text-center mb-10 ">
+            Comienza agregando pacientes {''}
+            <span className="text-indigo-600 font-bold  " >
+              y aparecerán en este lugar
+            </span>
+          </p>
+        
+        </>
+      ) }
+       
 
         
     </div>
